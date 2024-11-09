1349b315a3f554d9f7213c0178fbdf5ad1f0d6ec
venky <venky92195@gmail.com>
2024-11-09 18:21:20 +0530

    1.in domestic flights i changed to array then again undo it to normal 2.in Flight i added getters and setters for my objects and also some methods update,delete 3.in flightreservation i added some print statements and also some cases in switch,removed flagship

--- FlightReservationSystem/FlightAdmin.java
+++ FlightReservationSystem/FlightAdmin.java
@@ -1,11 +1,17 @@
 package FlightReservationSystem;
 
-import java.util.Scanner;
+
 
 public class FlightAdmin {
 
+
+
+
    protected static Flight domesticFlight = new DomesticFlights("A1324k","26/o6/2024",
            "26/06/2024","AirIndia",250,2000);
+
+
+
     protected static Flight internationalflight = new InternationalFlight("A1j234","9/06/2024",
             "11/06/2024","SaudiAirlines",550,4000);
 
